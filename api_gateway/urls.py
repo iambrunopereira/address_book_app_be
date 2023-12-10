07736b418e2170b6e3cb3630ffbfaac59bff2b60
Bruno Pereira <b.alexadre.p@outlook.com>
2023-12-10 12:02:27 +0000

    feat: added test end point to request data to from randomuser

--- api_gateway/urls.py
+++ api_gateway/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from .views import hello_world
+from .views import fetch_random_user
 
 urlpatterns = [
-    path('hello/', hello_world)
+    path('hello/', hello_world),
+    path('user/', fetch_random_user),
 ]
--- api_gateway/views.py
+++ api_gateway/views.py
@@ -6,3 +6,11 @@ from django.http import JsonResponse
 
 def hello_world(request):
     return JsonResponse({"message": "Hello World"})
+
+def fetch_random_user(request):
+    response = requests.get('https://randomuser.me/api/')
+    if response.status_code == 200:
+        data = response.json()
+        return JsonResponse(data)
+    else:
+        return JsonResponse({'error': 'Failed to fetch data'}, status=500)
