c450f012027181f2807cf823e9650e018dfcb56b
Bruno Pereira <b.alexadre.p@outlook.com>
2023-12-22 17:28:06 +0000

    feat: added api gateway endpoints

--- api_gateway/urls.py
+++ api_gateway/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path, include
-from .views import hello_world
-from .views import fetch_random_user
+
+from api_gateway.views import user_views
+from api_gateway.views import auth_views
+
+
 
 urlpatterns = [
-    path('hello/', hello_world),
-    path('user/', fetch_random_user),
+    path('user', user_views.fetch_user),
+    path('search', user_views.search_and_fetch),
+    path('user/add', user_views.add_to_favorites),
+    path('user/remove', user_views.remove_from_favorites),
+    path('user/favorites', user_views.get_favorites),
+    path('auth/login', auth_views.login),
+    path('auth/signup', auth_views.signup),
+    path('auth/change-password', auth_views.change_password),
+
 ]
